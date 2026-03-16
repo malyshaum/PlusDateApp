db37d0da173d5f835f81d7e6714792a782796f4b
malyshaum <mikita.malyshau@stud.esdc.lt>
2026-03-16 11:34:17 +0000

    feat: add current user endpoint

---
+++ api/database/migrations/2025_08_18_172649_update_user_feed_profile_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_feed_profile', function (Blueprint $table) {
+            $table->integer('height')->nullable();
+            $table->unsignedBigInteger('activity_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_feed_profile', function (Blueprint $table) {
+            $table->dropColumn('height');
+            $table->dropColumn('activity_id');
+        });
+    }
+};
